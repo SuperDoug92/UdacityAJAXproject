7c6626fe941c9e891a669a6c123b408a93d9da5e
Doug <clarke.doug248@gmail.com>
2016-07-05 18:04:56 -0400

    added imported articles from wikipedia API

--- js/script.js
+++ js/script.js
@@ -13,25 +13,48 @@ function loadData() {
     var htmlString = "<img class='bgimg' src='http://maps.googleapis.com/maps/api/streetview?size=600x400&location="+ $('#street').val() + ", " + $('#city').val() + "'>"
     $body.append(htmlString);
 
-    $.getJSON();
-
     var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     url += '?' + $.param({
       'api-key': "c811a5e8943745babf939f2ac8a52bd4",
-      'q': "Alexandria",
+      'q': $('#city').val(),
       'sort': "newest"
     });
 
     $.getJSON(url,function(data) {
-      console.log(data);
       data.response.docs.forEach(function(article){
         var htmlStr = "<a style='display:block' href='" + article.web_url +"'><h3>" + article.headline.main + "</h3><span>"+ article.byline.original + "</span><p>"+ article.lead_paragraph + "</p></a><hr>";
         $nytElem.append(htmlStr);
+      })
+    })
+    .fail(function(err) {
+      $nytElem.text("New York Times Articles could not be loaded");
+    });
+
+    var wikiURL = 'https://en.wikipedia.org/w/api.php'
+    wikiURL += '?' + $.param({
+      'action':'opensearch',
+      'search': $('#city').val(),
+      'format':'json',
+
+    });
+    // wikiURL += '&callback=?'
+
+
+    $.ajax( {
+      url: wikiURL,
+      dataType: 'jsonp',
+      headers: { 'Api-User-Agent': 'Example/1.0' },
+      success: function(data) {
+        data[1].forEach(function(result,index){
+          var HTMLStr = "<a style='display:block' href='" + data[3][index] +"'><h3>" +
+          result + "</h3><span>"+
+          data[2][index] + "</span></a><hr>";
+          $wikiElem.append(HTMLStr);
+        })
+      },
+      error: function(err){
+        console.log("JSONP failed");
       }
-    )
-      console.log(data.response.docs);
-    }).fail(function(err) {
-      throw err;
     });
 
     return false;
